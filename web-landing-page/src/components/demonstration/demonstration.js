50d549347194ed40462fb15adffc934ea7d79cd0
Lucas Lucena <dayyumn@gmail.com>
2020-08-31 17:12:40 -0300

    how-to section responsible and demonstration first adjustments responsiveness

--- web-landing-page/src/components/demonstration/demonstration.js
+++ web-landing-page/src/components/demonstration/demonstration.js
@@ -10,11 +10,13 @@ function Demonstrations() {
             </div>
             <div className="demonstration-body">
                 <div className="demonstration-body-content">
-                    <img src={require('../../assets/img/unnamed.png')} alt="celular demonstração"></img>
+                    <div className="celular-container">
+                        <img src={require('../../assets/img/unnamed.png')} alt="celular demonstração"></img>
+                    </div>
                     <div className="demonstration-body-content-text">
+                        <p>Occaecat eu exercitation sit pariatur sit ullamco ea eu do laboris ut. Ullamco mollit aute qui laborum fugiat adipisicing occaecat pariatur magna. Laborum magna irure et Lorem sit est mollit officia cillum deserunt veniam anim.
                         Occaecat eu exercitation sit pariatur sit ullamco ea eu do laboris ut. Ullamco mollit aute qui laborum fugiat adipisicing occaecat pariatur magna. Laborum magna irure et Lorem sit est mollit officia cillum deserunt veniam anim.
-                        Occaecat eu exercitation sit pariatur sit ullamco ea eu do laboris ut. Ullamco mollit aute qui laborum fugiat adipisicing occaecat pariatur magna. Laborum magna irure et Lorem sit est mollit officia cillum deserunt veniam anim.
-                        Occaecat eu exercitation sit pariatur sit ullamco ea eu do laboris ut. Ullamco mollit aute qui laborum fugiat adipisicing occaecat pariatur magna. Laborum magna irure et Lorem sit est mollit officia cillum deserunt veniam anim.
+                        </p>
                     </div>
                 </div>
             </div>
