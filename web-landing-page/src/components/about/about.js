4f2e5fc29022bb8be8e07a11284200a236685ed3
Lucas Lucena <dayyumn@gmail.com>
2020-08-31 15:34:43 -0300

    about section responsible

--- web-landing-page/src/components/about/about.js
+++ web-landing-page/src/components/about/about.js
@@ -14,32 +14,32 @@ function About() {
                     <div className="list-about">
                         <div className="list-about list-item list-item-one">
                             <img src={require('../../assets/img/2498326.png')}></img>
-                            <span>Aliquip esse dolor laboris veniam sint proident.</span>
+                            <span>Aliquip esse dolor</span>
                             <p>Anim pariatur aliquip laboris laboris sunt consequat duis fugiat. Incididunt irure nostrud culpa occaecat consectetur ullamco nostrud cupidatat nostrud quis cillum nulla mollit proident. Dolore ut sunt est proident et minim qui sunt duis nulla veniam ipsum. Exercitation dolor sint ut sint aliquip aliqua amet ut proident.</p>
                         </div>
                         <div className="list-about list-item list-item-two">
                             <img src={require('../../assets/img/business-and-finance (1).png')}></img>
-                            <span>Aliquip esse dolor laboris veniam sint proident.</span>
+                            <span>Sint proident</span>
                             <p>Anim pariatur aliquip laboris laboris sunt consequat duis fugiat. Incididunt irure nostrud culpa occaecat consectetur ullamco nostrud cupidatat nostrud quis cillum nulla mollit proident. Dolore ut sunt est proident et minim qui sunt duis nulla veniam ipsum. Exercitation dolor sint ut sint aliquip aliqua amet ut proident.</p>
                         </div>
                         <div className="list-about list-item list-item-three">
                             <img src={require('../../assets/img/bussiness.png')}></img>
-                            <span>Aliquip esse dolor laboris veniam sint proident.</span>
+                            <span>Laboris veniam sint proident</span>
                             <p>Anim pariatur aliquip laboris laboris sunt consequat duis fugiat. Incididunt irure nostrud culpa occaecat consectetur ullamco nostrud cupidatat nostrud quis cillum nulla mollit proident. Dolore ut sunt est proident et minim qui sunt duis nulla veniam ipsum. Exercitation dolor sint ut sint aliquip aliqua amet ut proident.</p>
                         </div>
                         <div className="list-about list-item list-item-four">
                             <img src={require('../../assets/img/investor.png')}></img>
-                            <span>Aliquip esse dolor laboris veniam sint proident.</span>
+                            <span>Aliquip</span>
                             <p>Anim pariatur aliquip laboris laboris sunt consequat duis fugiat. Incididunt irure nostrud culpa occaecat consectetur ullamco nostrud cupidatat nostrud quis cillum nulla mollit proident. Dolore ut sunt est proident et minim qui sunt duis nulla veniam ipsum. Exercitation dolor sint ut sint aliquip aliqua amet ut proident.</p>
                         </div>
                         <div className="list-about list-item list-item-five">
                             <img src={require('../../assets/img/money-bag.png')}></img>
-                            <span>Aliquip esse dolor laboris veniam sint proident.</span>
+                            <span>Aliquip</span>
                             <p>Anim pariatur aliquip laboris laboris sunt consequat duis fugiat. Incididunt irure nostrud culpa occaecat consectetur ullamco nostrud cupidatat nostrud quis cillum nulla mollit proident. Dolore ut sunt est proident et minim qui sunt duis nulla veniam ipsum. Exercitation dolor sint ut sint aliquip aliqua amet ut proident.</p>
                         </div>
                         <div className="list-about list-item list-item-six">
                             <img src={require('../../assets/img/problem-solving.png')}></img>
-                            <span>Aliquip esse dolor laboris veniam sint proident.</span>
+                            <span>Aliquip esse dolor laboris</span>
                             <p>Anim pariatur aliquip laboris laboris sunt consequat duis fugiat. Incididunt irure nostrud culpa occaecat consectetur ullamco nostrud cupidatat nostrud quis cillum nulla mollit proident. Dolore ut sunt est proident et minim qui sunt duis nulla veniam ipsum. Exercitation dolor sint ut sint aliquip aliqua amet ut proident.</p>
                         </div>
                     </div>
