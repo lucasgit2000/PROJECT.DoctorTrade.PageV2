a1e6087cdd145e45d193b98f4d0ba14f1f92ee9d
Lucas Lucena <dayyumn@gmail.com>
2020-08-31 15:24:55 -0300

    home responsible

--- web-landing-page/src/components/navbar/navbar.js
+++ web-landing-page/src/components/navbar/navbar.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './navbar.css'
 
 function NavBar() {
+
+    const [showMenuHamburguerList, setShowMenuHamburguerList] = useState(false)
+
+    let hamburgerMenuList = (
+        <ul className="hamburger-menu-list">
+            <li><a href="#instructions">Começar Agora</a></li>
+            <li><a href="#demonstration">Como Funciona</a></li>
+            <li><a href="#reviews">Reviews</a></li>
+            <li><a href="#portfolio">Produtos Doctor</a></li>
+            <li><a href="#about">Sobre</a></li>
+        </ul>
+    )
+
+    function ToggleMenuHamburguerList() {
+        setShowMenuHamburguerList(!showMenuHamburguerList)
+    }
+
     return (
         <div id="navBar" className="containerNavBar">
             <div className="contentNavBar">
@@ -12,13 +29,17 @@ function NavBar() {
                         src={require('../../assets/img/logo.png')}
                         alt='logo' />
                 </a>
-                <ul>
+                <button className="hamburger-menu" onClick={() => { ToggleMenuHamburguerList() }}>
+                    <i className="fa fa-bars"></i>
+                </button>
+                <ul className="containerNavBar-list">
                     <li><a href="#instructions">Começar Agora</a></li>
                     <li><a href="#demonstration">Como Funciona</a></li>
                     <li><a href="#reviews">Reviews</a></li>
                     <li><a href="#portfolio">Produtos Doctor</a></li>
                     <li><a href="#about">Sobre</a></li>
                 </ul>
+                {showMenuHamburguerList ? hamburgerMenuList : null}
             </div>
         </div>
     );
