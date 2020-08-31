340ab8fcd0ee90a2b36a19f016de8971da2c62e6
Lucas Lucena <dayyumn@gmail.com>
2020-08-31 15:36:59 -0300

    timer close button removed

--- web-landing-page/src/components/timer/timer.js
+++ web-landing-page/src/components/timer/timer.js
@@ -4,34 +4,14 @@ import './timer.css'
 
 function Timer() {
 
-    const [showTimer, setShowTimer] = useState(true)
-
-    function ShowTimer() {
-        setShowTimer(true)
-        console.log(showTimer)
-    }
-
-    function HideTimer() {
-        setShowTimer(false)
-        console.log(showTimer)
-    }
-
     return (
-
-        showTimer ?
-
         <div className="containerTimer">
-            <div className="timer-closeButton" onClick={() => {HideTimer()}}>x</div>
             <div className="contentTimer">
                 <span className="desc-promo">
                     🔥 CONTAGEM REGRESSIVA PARA FIM DAS INSCRIÇÕES DO <a href="#">CURSO</a>: <strong>10:00:00</strong>
                 </span>
             </div>
         </div>
-
-        :
-
-        null
     );
 }
 
